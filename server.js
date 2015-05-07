aedacf00393db3c45f3c4f96d42fa8924bd47e41
Mats Byrkjeland <matsbyr@gmail.com>
2015-05-07 18:54:02 +0200

    Add CORS headers

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ mongoose.connect('mongodb://localhost/test');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Routes
 app.use('/api', require('./routes/api'));
