272f19f5aa4ad65ed3b05c1ba5ed297973e7e92a
Mats Byrkjeland <matsbyr@gmail.com>
2016-01-20 23:08:14 -0800

    Replace deprecated  service success method with standard then method

--- client/scripts/services/http_request.js
+++ client/scripts/services/http_request.js
@@ -9,41 +9,37 @@ angular.module('kramster')
 
         // Gets an array and forwards it to callback function. If order is given, sort data in that order.
         this.get = function(url, callback) {
-            $http.get(url).
-                success(function(data) {
-                    callback(data);
-                });
+            $http.get(url).then(function(res) {
+                callback(res.data);
+            });
         };
 
         // Gets the selected document and passes on to callback.
         this.getSelected = function(url, callback) {
             $rootScope.loading = true;
-            $http.get(url)
-                .success(function(data) {
-                    $rootScope.loading = false;
-                    callback(data);
+            $http.get(url).then(function(res) {
+                $rootScope.loading = false;
+                callback(res.data);
             });
         };
 
         // Gets all questions of all documents of given url and passes to callback. Passes also a meta object.
         this.getAll = function(url, callback) {
             $rootScope.loading = true;
-            $http.get(url)
-                .success(function(data) {
-                    $rootScope.loading = false;
-                    var allQuestions = [];
-                    for (var i=0; i < data.length; i++) {
-                        allQuestions.push.apply(allQuestions, data[i].questions);
-                    }
+            $http.get(url).then(function(res) {
+                $rootScope.loading = false;
+                var allQuestions = [];
+                for (var i = 0; i < res.data.length; i++) {
+                    allQuestions.push.apply(allQuestions, res.data[i].questions);
+                }
 
-                    var uniqueModes = helpers.removeDuplicates(data.map(function(doc) {return doc.mode}));
+                var uniqueModes = helpers.removeDuplicates(res.data.map(function(doc) {return doc.mode}));
 
-                    var meta = {
-                        mode: (uniqueModes.length === 1) ? uniqueModes[0] : 'MC'
-                    };
-
-                    callback(allQuestions, meta);
-                });
+                var meta = {
+                    mode: (uniqueModes.length === 1) ? uniqueModes[0] : 'MC'
+                };
+                callback(allQuestions, meta);
+            });
         };
 
         // General http POST
