544a2f83d28d420e647bfd61d5d15c89c964dbf6
Mats Byrkjeland <matsbyr@gmail.com>
2016-02-01 19:01:44 -0800

    Add controller to navbar directive, change some colors

--- client/scripts/directives/navbar.js
+++ client/scripts/directives/navbar.js
@@ -2,8 +2,16 @@
 
 angular.module('kramster')
     .directive('navbar', function () {
+
+        var controller = ['$scope', '$location', function ($scope, $location) {
+            $scope.onRootLocation = function () {
+                return $location.url() === '/';
+            };
+        }];
+
         return {
             restrict: 'E',
-            templateUrl: '../../views/navbar.html'
+            templateUrl: '../../views/navbar.html',
+            controller: controller
         };
     });
