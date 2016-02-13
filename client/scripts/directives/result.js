4c20955a7eb7f31a1af1cc899b1927929348c5b5
Mats Byrkjeland <matsbyr@gmail.com>
2016-02-12 17:54:21 -0800

    Remove moved duplicated files

--- client/scripts/directives/result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('kramster')
-    .directive('result', function () {
-        return {
-            restrict: 'E',
-            templateUrl: '../../views/result.html'
-        };
-    });
