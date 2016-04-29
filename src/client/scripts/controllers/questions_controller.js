57cc7f6527d0a1e50fe0024773357884338992a3
Mats Byrkjeland <matsbyr@gmail.com>
2016-04-29 01:51:42 -0700

    Fix score bug

--- src/client/scripts/controllers/questions_controller.js
+++ src/client/scripts/controllers/questions_controller.js
@@ -176,7 +176,7 @@ angular.module('kramster')
 
         // Number of correct answers
         score: function () {
-          return app.numAnswered();
+          return app.numCorrects();
         },
       };
 
