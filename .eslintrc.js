6654efb8467f315c9d6da1fee1c84f68105ddef5
Mats Byrkjeland <matsbyr@gmail.com>
2016-11-17 00:35:02 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "es6": true,
+    },
+    "extends": [
+      "airbnb-base"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+};
---
+++ src/client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": [
+    "angular"
+  ],
+  "extends": "angular",
+  "env": {
+    "browser": true,
+  },
+  "rules": {
+    "no-param-reassign": "off",
+  }
+};
