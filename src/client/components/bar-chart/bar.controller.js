b3e040a8cd1dfada594cf3ef86147528bf990a49
Mats Byrkjeland <matsbyr@gmail.com>
2017-01-05 16:16:29 +0100

    Fix charts on result page

--- src/client/components/bar-chart/bar.controller.js
+++ src/client/components/bar-chart/bar.controller.js
@@ -1,61 +1,63 @@
 /* eslint-disable angular/controller-as */
 angular.module('kramster')
-  .controller('BarController', ['$scope', 'httpRequest', function BarController($scope, httpRequest) {
-    const baseUrl = '/api/stats/';
-    const r = $scope.$parent.$parent.route;
-    const mode = $scope.$parent.$parent.mode;
-    const dataUrl = r
-      ? `${baseUrl + r.school}/${r.course}/${r.exam || mode.docMode}`
-      : baseUrl;
+  .controller('BarController', ['$scope', '$routeParams', '$route', 'httpRequest',
+    function BarController($scope, $routeParams, $route, httpRequest) {
+      const baseUrl = '/api/stats/';
+      const r = $routeParams;
+      const mode = $route.current.locals.mode;
 
-    const params = {};
-    if (mode && mode.docMode && mode.docMode === 'random') {
-      params.numQuestions = $scope.numberOfQuestions();
-    }
+      const dataUrl = r && Object.keys(r).length > 0
+        ? `${baseUrl + r.school}/${r.course}/${r.exam || mode}`
+        : baseUrl;
 
-    $scope.labels = ['A', 'B', 'C', 'D', 'E', 'F'];
-    $scope.data = [[]];
-
-    $scope.colors = [{ fillColor: '#e74c3c' }];
-
-    $scope.options = {
-      scaleOverride: true,
-      scaleStartValue: 0,
-      scaleSteps: 0,
-      scaleStepWidth: 1,
-      scaleShowGridLines: false,
-      scaleShowHorizontalLines: false,
-      scaleShowVerticalLines: false,
-      showScale: false,
-      barShowStroke: false,
-      barValueSpacing: 5,
-      maintainAspectRatio: true,
-    };
-
-    httpRequest.get(dataUrl, params, (res) => {
-      const data = [];
-      for (let i = 0; i < $scope.labels.length; i++) {
-        data.push(res.grades[$scope.labels[i]]);
+      const params = {};
+      if (mode && mode.docMode && mode.docMode === 'random') {
+        params.numQuestions = $scope.numberOfQuestions();
       }
 
-      $scope.data[0] = data;
+      $scope.labels = ['A', 'B', 'C', 'D', 'E', 'F'];
+      $scope.data = [[]];
+
+      $scope.colors = [{ fillColor: '#e74c3c' }];
+
+      $scope.options = {
+        scaleOverride: true,
+        scaleStartValue: 0,
+        scaleSteps: 0,
+        scaleStepWidth: 1,
+        scaleShowGridLines: false,
+        scaleShowHorizontalLines: false,
+        scaleShowVerticalLines: false,
+        showScale: false,
+        barShowStroke: false,
+        barValueSpacing: 5,
+        maintainAspectRatio: false,
+      };
+
+      httpRequest.get(dataUrl, params, (res) => {
+        const data = [];
+        for (let i = 0; i < $scope.labels.length; i++) {
+          data.push(res.grades[$scope.labels[i]]);
+        }
+
+        $scope.data[0] = data;
 
       // Update scale start value and number of steps
-      let minVal = data[0];
-      let maxVal = data[0];
-      for (let i = 0; i < data.length; i++) {
-        if (data[i] < minVal) {
-          minVal = data[i];
+        let minVal = data[0];
+        let maxVal = data[0];
+        for (let i = 0; i < data.length; i++) {
+          if (data[i] < minVal) {
+            minVal = data[i];
+          }
+
+          if (data[i] > maxVal) {
+            maxVal = data[i];
+          }
         }
 
-        if (data[i] > maxVal) {
-          maxVal = data[i];
-        }
-      }
-
-      const minColHeight = Math.max(1, Math.round((maxVal - minVal) / 10));
-      $scope.options.scaleStartValue = (minVal <= minColHeight) ? 0 : minVal - minColHeight;
-      $scope.options.scaleSteps = (maxVal - minVal) + minColHeight;
-    });
-  },
+        const minColHeight = Math.max(1, Math.round((maxVal - minVal) / 10));
+        $scope.options.scaleStartValue = (minVal <= minColHeight) ? 0 : minVal - minColHeight;
+        $scope.options.scaleSteps = (maxVal - minVal) + minColHeight;
+      });
+    },
   ]);
--- src/client/components/pie-chart/pie.controller.js
+++ src/client/components/pie-chart/pie.controller.js
@@ -1,36 +1,38 @@
 /* eslint-disable angular/controller-as */
 
 angular.module('kramster')
-  .controller('PieController', ['$scope', 'httpRequest', function PieController($scope, httpRequest) {
-    const baseUrl = '/api/stats/';
-    const r = $scope.$parent.$parent.route;
-    const mode = $scope.$parent.$parent.mode;
-    const dataUrl = r
-      ? `${baseUrl + r.school}/${r.course}/${r.exam || mode.docMode}`
-      : baseUrl;
+  .controller('PieController', ['$scope', '$routeParams', '$route', 'httpRequest',
+    function PieController($scope, $routeParams, $route, httpRequest) {
+      const baseUrl = '/api/stats/';
+      const r = $routeParams;
+      const mode = $route.current.locals.mode;
 
-    const params = {};
-    if (mode && mode.docMode && mode.docMode === 'random') {
-      params.numQuestions = $scope.numberOfQuestions();
-    }
+      const dataUrl = r && Object.keys(r).length > 0
+        ? `${baseUrl + r.school}/${r.course}/${r.exam || mode}`
+        : baseUrl;
 
-    $scope.labels = ['A', 'B', 'C', 'D', 'E', 'F'];
-
-    $scope.options = {
-      segmentShowStroke: false,
-      percentageInnerCutout: 0,
-      animationEasing: 'easeOutBounce',
-      animateRotate: true,
-      animateScale: false,
-    };
-
-    httpRequest.get(dataUrl, params, (res) => {
-      const data = [];
-      for (let i = 0; i < $scope.labels.length; i++) {
-        data.push(res.grades[$scope.labels[i]]);
+      const params = {};
+      if (mode && mode.docMode && mode.docMode === 'random') {
+        params.numQuestions = $scope.numberOfQuestions();
       }
 
-      $scope.data = data;
-    });
-  },
+      $scope.labels = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+      $scope.options = {
+        segmentShowStroke: false,
+        percentageInnerCutout: 0,
+        animationEasing: 'easeOutBounce',
+        animateRotate: true,
+        animateScale: false,
+      };
+
+      httpRequest.get(dataUrl, params, (res) => {
+        const data = [];
+        for (let i = 0; i < $scope.labels.length; i++) {
+          data.push(res.grades[$scope.labels[i]]);
+        }
+
+        $scope.data = data;
+      });
+    },
   ]);
