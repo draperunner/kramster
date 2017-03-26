ba5fa3048de0335b799767e27fe477153dcf8bf3
Mats Byrkjeland <matsbyr@gmail.com>
2017-03-26 18:40:09 +0200

    Add Google Analytics

--- src/client/components/google-analytics/tracker.controller.js
+++
@@ -1,9 +0,0 @@
-/* Google Analytics */
-angular.module('kramster')
-    .controller('TrackerController', ['$scope', '$location', '$window',
-      function TrackerController($scope, $location, $window) {
-        $scope.$on('$viewContentLoaded', () => {
-          $window.ga('send', 'pageview', { page: $location.url() });
-        });
-      },
-    ]);
--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import ReactGA from 'react-ga';
 
 import AppContainer from './containers/AppContainer';
 import About from './containers/About';
@@ -9,8 +10,15 @@ import Exams from './containers/Exams';
 import Questions from './containers/Questions';
 import Result from './containers/Result';
 
+ReactGA.initialize('UA-62911300-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 export default
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     <Route path="/" component={AppContainer}>
       <IndexRoute component={Schools} />
       <Route path="/about" component={About} />
