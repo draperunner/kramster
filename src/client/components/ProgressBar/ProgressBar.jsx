5140349a549676d2ce5d9fd1046f60c54845cf09
Mats Byrkjeland <matsbyr@gmail.com>
2017-04-24 19:17:20 +0200

    Add white background to progress bar and progress counter

--- src/client/components/ProgressBar/ProgressBar.jsx
+++ src/client/components/ProgressBar/ProgressBar.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 
 const ProgressBar = (props) => {
-  const value = () => (props.questions.length > 0 ? Math.floor(10000 / props.questions.length) / 100 : 0);
+  const value = props.questions.length > 0 ? 100 / props.questions.length : 0;
   const type = index => (props.history[index] ? 'correct' : 'wrong');
 
   return (
-    <div className="progress">
-      { props.history.map((answer, i) =>
-        <div
-          key={i}
-          className={`progress-bar progress-bar-${type(i)}`}
-          aria-valuenow={value()}
-          aria-valuemin="0"
-          aria-valuemax="100"
-          aria-valuetext={`${value()}`}
-          style={{ width: `${Math.min(value(), 100)}%` }}
-          role="progressbar"
-        />,
+    <div>
+      <p className="progress-text">{`${props.history.length}/${props.questions.length}`}</p>
+      <div className="progress">
+        { props.history.map((answer, i) =>
+          <div
+            key={i}
+            className={`progress-bar progress-bar-${type(i)}`}
+            aria-valuenow={value}
+            aria-valuemin="0"
+            aria-valuemax="100"
+            aria-valuetext={`${value}`}
+            style={{ width: `${value}%` }}
+            role="progressbar"
+          />,
     )}
+      </div>
     </div>
   );
 };
 
 ProgressBar.propTypes = {
   history: React.PropTypes.arrayOf(React.PropTypes.bool),
+  questions: React.PropTypes.arrayOf(React.PropTypes.object),
 };
 
 export default ProgressBar;
