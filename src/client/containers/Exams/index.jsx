076f20aa91a185ce3ba79ce8c15ef89b3f4a7858
Mats Byrkjeland <matsbyr@gmail.com>
2017-05-16 16:25:52 +0200

    Put exams in more columns on mobile

--- src/client/containers/Exams/index.jsx
+++ src/client/containers/Exams/index.jsx
@@ -31,20 +31,20 @@ class Exams extends React.Component {
     return (
       <div className={styles.wrapper}>
         <Row className={styles.categoriesRow}>
-          <Col xs={12} sm={4} className={styles.col}>
+          <Col xs={4} className={styles.col}>
             <CategoryButton href={`/${this.state.school}/${this.state.course}/random/10`}>10 Random</CategoryButton>
           </Col>
-          <Col xs={12} sm={4} className={styles.col}>
+          <Col xs={4} className={styles.col}>
             <CategoryButton href={`/${this.state.school}/${this.state.course}/random/30`}>30 Random</CategoryButton>
           </Col>
-          <Col xs={12} sm={4} className={styles.col}>
+          <Col xs={4} className={styles.col}>
             <CategoryButton href={`/${this.state.school}/${this.state.course}/hardest/10`}>10 Hardest</CategoryButton>
           </Col>
         </Row>
 
         <Row className={styles.examsRow}>
           { this.state.exams.map(exam => (
-            <Col key={exam} xs={12} sm={6} md={4} lg={3} className={styles.col}>
+            <Col key={exam} xs={6} md={4} lg={3} className={styles.col}>
               <StandardButton href={`/${this.state.school}/${this.state.course}/${exam}`}>{ exam }</StandardButton>
             </Col>
           ))}
