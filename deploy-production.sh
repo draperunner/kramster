8bc7fbcfed6c4b5321fb571c77b349ac4ac1a920
Mats Byrkjeland <matsbyr@gmail.com>
2018-11-29 17:31:33 +0100

    Add Firebase setup

--- deploy-production.sh
+++
@@ -1,7 +0,0 @@
-npm run build
-echo "Compressing..."
-tar -zcf dist.tar.gz dist package.json
-echo "Sending..."
-scp dist.tar.gz kramster:dist/
-echo "Running server prod deploy script"
-ssh -t kramster './deploy-production.sh'
--- deploy-staging.sh
+++
@@ -1,7 +0,0 @@
-npm run build
-echo "Compressing..."
-tar -zcf stage.tar.gz dist package.json
-echo "Sending..."
-scp stage.tar.gz kramster:stage/
-echo "Running server staging deploy script"
-ssh -t kramster './deploy-staging.sh'
