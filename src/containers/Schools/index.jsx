86786424c053055ef34ea65bc287c76bc1d5844f
Mats Byrkjeland <matsbyr@gmail.com>
2019-05-07 22:52:29 +0200

    Use hooks in Schools

--- src/containers/Schools/index.jsx
+++ src/containers/Schools/index.jsx
@@ -1,54 +1,39 @@
 import React from 'react'
 import { browserHistory } from 'react-router'
 import { Row, Col } from 'react-flexbox-grid'
-import { getSchools, getStats } from '../../api'
+import { useSchools, useStats } from '../../hooks'
 import Jumbotron from '../../components/Jumbotron'
 import Kitem from '../../components/Kitem'
 import { header, name } from './methods'
 import styles from './Schools.css'
 
-class Schools extends React.Component {
-  constructor() {
-    super()
+function Schools() {
+  const schools = useSchools([
+    'Norges Teknisk-Naturvitenskaplige Universitet (NTNU)',
+    'Storbyuniversitetet (OsloMet)',
+  ])
 
-    this.state = {
-      gradesData: null,
-      schools: [
-        'Norges Teknisk-Naturvitenskaplige Universitet (NTNU)',
-        'Storbyuniversitetet (OsloMet)',
-      ],
-    }
-  }
+  const stats = useStats()
+  const grades = stats ? stats.grades : null
 
-  componentDidMount() {
-    getStats().then((data) => {
-      this.setState({ gradesData: data.grades })
-    })
-    getSchools().then((data) => {
-      this.setState({ schools: data })
-    })
-  }
+  return (
+    <div>
+      <Jumbotron gradesData={grades} />
 
-  render() {
-    return (
-      <div>
-        <Jumbotron gradesData={this.state.gradesData} />
-
-        <Row className={styles.schoolsRow}>
-          {this.state.schools.map(school => (
-            <Col xs={12} sm={6} lg={3} key={school}>
-              <Kitem
-                head={header(school)}
-                body={name(school)}
-                color="green"
-                onClick={() => browserHistory.push(`/${header(school)}`)}
-              />
-            </Col>
-          ))}
-        </Row>
-      </div>
-    )
-  }
+      <Row className={styles.schoolsRow}>
+        {schools.map(school => (
+          <Col xs={12} sm={6} lg={3} key={school}>
+            <Kitem
+              head={header(school)}
+              body={name(school)}
+              color="green"
+              onClick={() => browserHistory.push(`/${header(school)}`)}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
 }
 
 export default Schools
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,28 @@
 import { useState, useEffect } from 'react'
 
-import { getCourses, getExams } from '../api'
+import {
+  getStats, getSchools, getCourses, getExams,
+} from '../api'
+
+export function useStats() {
+  const [stats, setStats] = useState()
+
+  useEffect(() => {
+    getStats().then(setStats)
+  }, [])
+
+  return stats
+}
+
+export function useSchools(initialSchools) {
+  const [schools, setSchools] = useState(initialSchools)
+
+  useEffect(() => {
+    getSchools().then(setSchools)
+  }, [])
+
+  return schools
+}
 
 export function useCourses(school) {
   const [courses, setCourses] = useState([])
