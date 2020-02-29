ecf6b614ca00aca98a38904314794c2b54aef6a3
Mats Byrkjeland <matsbyr@gmail.com>
2020-02-29 18:53:16 +0100

    Autoformat tests and webpack config

--- test/exam-api-tests.js
+++ test/exam-api-tests.js
@@ -2,83 +2,83 @@
  * Created by mats on 3/31/16.
  */
 
-var supertest = require('supertest');
-var should = require('should');
-var mocha = require('mocha');
+const supertest = require('supertest')
+const should = require('should')
+const mocha = require('mocha')
 
-var server = supertest.agent('http://localhost:8000/api');
+const server = supertest.agent('http://localhost:8000/api')
 
-var expectJsonStatus200 = function (url, done) {
+const expectJsonStatus200 = function(url, done) {
   server
     .get(url)
     .expect('Content-type', /json/)
     .expect(200)
-    .end(function (err, res) {
-      if (err) return done(err);
-      res.status.should.equal(200);
-      done();
-    });
-};
+    .end(function(err, res) {
+      if (err) return done(err)
+      res.status.should.equal(200)
+      done()
+    })
+}
 
-var expect404 = function (url, done) {
+const expect404 = function(url, done) {
   server
     .get(url)
     .expect(404)
-    .end(function (err, res) {
-      if (err) return done(err);
-      res.status.should.equal(404);
-      done();
-    });
-};
+    .end(function(err, res) {
+      if (err) return done(err)
+      res.status.should.equal(404)
+      done()
+    })
+}
 
-describe('All exams', function () {
+describe('All exams', function() {
+  it('should return all exams', function(done) {
+    expectJsonStatus200('/exams', done)
+  })
+})
 
-  it('should return all exams', function (done) {
-    expectJsonStatus200('/exams', done);
-  });
+describe('Exams for single school', function() {
+  it('ntnu', function(done) {
+    expectJsonStatus200('/exams/ntnu', done)
+  })
 
-});
+  it('NTNU', function(done) {
+    expectJsonStatus200('/exams/NTNU', done)
+  })
 
-describe('Exams for single school', function () {
-  it('ntnu', function (done) {
-    expectJsonStatus200('/exams/ntnu', done);
-  });
+  it('Norges Teknisk-Naturvitenskaplige Universitet (NTNU)', function(done) {
+    expectJsonStatus200(
+      '/exams/Norges Teknisk-Naturvitenskaplige Universitet (NTNU)',
+      done,
+    )
+  })
 
-  it('NTNU', function (done) {
-    expectJsonStatus200('/exams/NTNU', done);
-  });
+  it('nhh', function(done) {
+    expectJsonStatus200('/exams/nhh', done)
+  })
 
-  it('Norges Teknisk-Naturvitenskaplige Universitet (NTNU)', function (done) {
-    expectJsonStatus200('/exams/Norges Teknisk-Naturvitenskaplige Universitet (NTNU)', done);
-  });
+  it('NHH', function(done) {
+    expectJsonStatus200('/exams/NHH', done)
+  })
 
-  it('nhh', function (done) {
-    expectJsonStatus200('/exams/nhh', done);
-  });
+  it('Norges Handelshøyskole (NHH)', function(done) {
+    expectJsonStatus200('/exams/Norges Handelshøyskole (NHH)', done)
+  })
 
-  it('NHH', function (done) {
-    expectJsonStatus200('/exams/NHH', done);
-  });
+  it('Foobar University', function(done) {
+    expect404('/exams/Foobar University', done)
+  })
 
-  it('Norges Handelshøyskole (NHH)', function (done) {
-    expectJsonStatus200('/exams/Norges Handelshøyskole (NHH)', done);
-  });
-
-  it('Foobar University', function (done) {
-    expect404('/exams/Foobar University', done);
-  });
-
-  it('NTNU?limit=1', function (done) {
+  it('NTNU?limit=1', function(done) {
     server
       .get('/exams/NTNU?limit=1')
       .expect('Content-type', /json/)
       .expect(200)
-      .end(function (err, res) {
-        if (err) return done(err);
-        res.status.should.equal(200);
-        res.body.length.should.be.exactly(1);
-        done();
-      });
-  });
-
-});
+      .end(function(err, res) {
+        if (err) return done(err)
+        res.status.should.equal(200)
+        res.body.length.should.be.exactly(1)
+        done()
+      })
+  })
+})
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,21 +2,18 @@ const webpack = require('webpack')
 const webpackMerge = require('webpack-merge')
 const commonConfig = require('./webpack.config')
 
-module.exports = () => webpackMerge(commonConfig(), {
-  mode: 'development',
-  entry: [
-    'webpack-hot-middleware/client',
-  ],
-  output: {
-    // path: path.resolve(__dirname, 'src'),
-    path: '/',
-    filename: 'bundle.[hash].js',
-  },
-  devServer: {
-    historyApiFallback: true,
-    open: true,
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-})
+module.exports = () =>
+  webpackMerge(commonConfig(), {
+    mode: 'development',
+    entry: ['webpack-hot-middleware/client'],
+    output: {
+      // path: path.resolve(__dirname, 'src'),
+      path: '/',
+      filename: 'bundle.[hash].js',
+    },
+    devServer: {
+      historyApiFallback: true,
+      open: true,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+  })
