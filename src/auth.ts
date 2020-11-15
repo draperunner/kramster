31617306ebccd7597d631544ab546097ab775f15
Mats Byrkjeland <matsbyr@gmail.com>
2020-11-15 21:57:44 +0100

    Setup config for staging and prod

--- src/auth.ts
+++ src/auth.ts
@@ -4,15 +4,7 @@ import firebase from 'firebase/app'
 import 'firebase/analytics'
 import 'firebase/auth'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyDubEbkaUX-Yyx4b1ZYYHZ_ba5sEi9aPbU',
-  authDomain: 'kramster-staging.firebaseapp.com',
-  databaseURL: 'https://kramster-staging.firebaseio.com',
-  projectId: 'kramster-staging',
-  storageBucket: 'kramster-staging.appspot.com',
-  messagingSenderId: '591159005378',
-  appId: '1:591159005378:web:fa1e2ed1914ec3f27f2b5b',
-})
+firebase.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG || ''))
 
 export function useAnonymousLogin(): firebase.User | null | undefined {
   const [user, setUser] = useState<firebase.User | null | undefined>()
