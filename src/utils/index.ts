ed6005f3770859dacd18917162f8566823427ec7
Mats Byrkjeland <git@byrkje.land>
2025-02-11 21:11:42 +0100

    Remove unused utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,14 +15,6 @@ export function formatPercentage(dividend: number, divisor: number): number {
   return Math.round((10000 * dividend) / divisor) / 100;
 }
 
-export function formatStatusMessage(score: number, percentage: number): string {
-  return `${score.toFixed(2)} (${percentage}%)`;
-}
-
-export function reloadRoute(): void {
-  window.location.reload();
-}
-
 export function percentageToGrade(percentage: number): Grade {
   const scale = [89, 77, 65, 53, 41];
   const grades: Array<Grade> = ["A", "B", "C", "D", "E"];
@@ -33,41 +25,6 @@ export function percentageToGrade(percentage: number): Grade {
   }
 
   return "F";
-}
-
-export function shuffle<T>(array: Array<T>): Array<T> {
-  const size = array.length;
-  const arr = [...array];
-  for (let i = 0; i < size; i++) {
-    const j = Math.round(i + (size - 1 - i) * Math.random());
-    const temp = array[i];
-    arr[i] = array[j];
-    arr[j] = temp;
-  }
-
-  return arr;
-}
-
-export function findSubstringEnclosedInParenthesis(
-  s: string,
-): RegExpExecArray | null {
-  return /\(([^)]+)\)/.exec(s);
-}
-
-export function getLocalTime(): string {
-  const now = new Date();
-  const timezoneOffset = -now.getTimezoneOffset();
-  const sign = timezoneOffset >= 0 ? "+" : "-";
-  const pad = (num: number): string => {
-    const norm = Math.abs(Math.floor(num));
-    return (norm < 10 ? "0" : "") + norm;
-  };
-
-  return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(
-    now.getDate(),
-  )}T${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(
-    now.getSeconds(),
-  )}${sign}${pad(timezoneOffset / 60)}:${pad(timezoneOffset % 60)}`;
 }
 
 export function sanitize(dirtyHtml: string): string {
