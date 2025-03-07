d7dae3630203fc5e9b34ef924507a39901473400
Mats Byrkjeland <git@byrkje.land>
2025-03-07 21:41:22 +0100

    Add service worker for offline support
    If the web app is installed (added to home screen), it and the exams you
    have visited are stored on the device so you can cram offline.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,70 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["**"],
+      manifest: {
+        name: "Kramster!",
+        short_name: "Kramster!",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ecf0f1",
+        theme_color: "#ecf0f1",
+        description: "Cram for your exam with Kramster!",
+        orientation: "portrait",
+        lang: "no",
+        icons: [
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
